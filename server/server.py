ce9b51aa4fda222d095997e415784b4378f79265
Mark Côté <mcote@alumni.uwaterloo.ca>
2013-04-18 10:53:30 -0400

    Bug 863188 - settings.cfg option to enable debug mode. r=bc

--- server/server.py
+++ server/server.py
@@ -1,7 +1,15 @@
+import ConfigParser
 import templeton.handlers
 import templeton.middleware
 import handlers
 import web
+
+config = ConfigParser.ConfigParser()
+config.read('settings.cfg')
+try:
+    web.config.debug = config.getboolean('server', 'debug')
+except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+    web.config.debug = False
 
 urls = templeton.handlers.load_urls(handlers.urls)
 
